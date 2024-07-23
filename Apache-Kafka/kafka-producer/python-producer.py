b51d084babb5a659d8a7ef5aebc1c9f8941ac2e0
Long Nguyen <zedsc03@gmail.com>
2024-07-23 20:37:18 +0700

    define kafka producer service

---
+++ Apache-Kafka/kafka-producer/python-producer.py
@@ -0,0 +1,37 @@
+import json
+import time
+import logging
+import schedule
+import pandas as pd
+from faker import Faker
+from time import sleep
+from json import dumps
+
+from kafka import KafkaProducer
+
+
+
+
+def generate_data() -> None:
+    
+    fakeDataTestingObject = Faker()
+    # Connect kafka and create kafka producer object
+    producer = KafkaProducer(bootstrap_servers=["kafka:9092"], 
+        value_serializer=lambda m: json.dumps(m).encode("utf-8")
+    )
+    my_FakeData = {"sentence": fakeDataTestingObject.sentence()}
+    logging.info(f"Data fake using in proj: {my_FakeData}")
+    producer.send(topic="sentence", value=my_FakeData)
+    producer.flush()
+    
+
+# producer = KafkaProducer(topic='sentences', bootstrap_servers=['kafka:9092'],
+#     value_serializer=lambda m: dumps(m).encode('utf-8'))
+# producer.send(topic='sentences', value={'surnasdasdame': 'parasdasdmar'})
+
+# dataframe = pd.read_csv(filepath_or_buffer='data/indexProcessed.csv')
+
+
+if __name__ == '__main__':
+    time.sleep(5)
+    generate_data()
