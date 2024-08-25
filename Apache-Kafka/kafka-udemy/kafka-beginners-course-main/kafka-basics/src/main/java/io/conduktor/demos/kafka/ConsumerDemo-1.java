6a91451633531770785a39a7ab16bfe4a2f46bde
Long Nguyen <zedsc03@gmail.com>
2024-08-25 22:19:50 +0700

    Add source code

---
+++ Apache-Kafka/kafka-udemy/kafka-beginners-course-main/kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo-1.java
@@ -0,0 +1,6 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
